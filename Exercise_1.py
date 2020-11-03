9db3e8aed2339fd9fb32096c06f6fd37580d462a
tsaqafi <73459739+tsaqafi@users.noreply.github.com>
2020-11-03 20:17:21 +0800

    Delete file

--- Exercise_1.py
+++
@@ -1,8 +0,0 @@
-# Calculate rainfall in gallons for some number of inches on 1 acre.
-
-inches_str = input("How many inches of rain has fallen: ")
-inches_int = int(inches_str)
-volume =(inches_int/12)*43560
-gallons = volume*7.48051945
-
-print(inches_int, "inches rain on 1 acre is", gallons, "gallons.")
